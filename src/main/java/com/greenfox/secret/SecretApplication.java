7d75ea6a003e091e5c4c5a12db469fc79ff03858
barbarasimandi <barbarasimandi@gmail.com>
2017-12-01 11:02:15 +0100

    init commit, guys

---
+++ src/main/java/com/greenfox/secret/SecretApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.secret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretApplication.class, args);
+	}
+}
